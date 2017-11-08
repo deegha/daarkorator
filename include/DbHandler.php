0fb9dbab82296eb37dca178e7afbdfd4fe4a4411
Deegha Galkissa <00T884@ual.lk>
2017-11-08 17:15:56 +0530

    added budget and room types to the project list

--- include/DbHandler.php
+++ include/DbHandler.php
@@ -635,7 +635,7 @@ class DbHandler {
             $db           = new database();
             $table        = "project p join project_details pd on p.id = pd.project_id 
                              join room_types rt on pd.room_types = rt.id";
-            $rows         = "p.*, rt.image, rt.title, pd.budget,
+            $rows         = "p.*, rt.image, rt.title room_type, pd.budget as budget,
                                 case p.status WHEN 0 then 'Draft' WHEN 1 then 'In progress' WHEN 2 then 'Finalized' WHEN 3 then 'Completed' WHEN 4 then 'Canceled' END AS status_title, 
                                 pd.title";
             $where        = ""; 
@@ -643,7 +643,7 @@ class DbHandler {
                 $table      = "project p join project_details pd on p.id = pd.project_id
                                 join room_types rt on pd.room_types = rt.id";
                 $where      = "p.id NOT IN (select project_id from daakor_project where daakor_id = ".$user_id.") and status = 1";
-                $rows       = "p.*,rt.title, pd.budget, case p.status WHEN 1 then 'In progress' END AS status_title, pd.title, rt.image ";
+                $rows       = "p.*,rt.title as room_type, pd.budget as budget, case p.status WHEN 1 then 'In progress' END AS status_title, pd.title, rt.image ";
         
             }else{ 
                 if($logged_user_type == 3 ){  
@@ -653,7 +653,7 @@ class DbHandler {
                                     join project_details pd 
                                     on p.id = pd.project_id
                                     join room_types rt on pd.room_types = rt.id";
-                    $rows         = "p.*, rt.image, rt.title, pd.budget,
+                    $rows         = "p.*, rt.image, rt.title as room_type, pd.budget as budget,
                                     case p.status WHEN 0 then 'Draft' WHEN 1 then 'In progress' WHEN 2 then 'Won' WHEN 3 then 'Completed' WHEN 4 then 'Canceled' END AS status_title, 
                                         pd.title" ;
                     $where = "dp.daakor_id=".$user_id." and p.status <> 3 and p.status <> 4 and p.status <> 0";
@@ -943,7 +943,7 @@ class DbHandler {
                         pd.title as project_name,
                         psb.style_board_name as styleboard_name,
                         m.status as status";
-            $where  = "m.reciever_id = ".$user_id;
+            $where  = "m.reciever_id = ".$user_id." OR m.sender_id = ".$user_id;
 
             if(isset($status))
             $where .= " AND m.status = ".$status;
